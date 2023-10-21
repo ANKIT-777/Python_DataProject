39ae04003340982d5239e071551af56dfacc3537
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-21 17:04:49 +0530

    updated file

--- IPL/MatchesPlayedPerTeamPerseason.py
+++ IPL/MatchesPlayedPerTeamPerseason.py
@@ -1,47 +1,51 @@
 import csv
 import matplotlib.pyplot as plt
-with open('IPL/matches.csv') as csvfile:
-    DataSet = csv.DictReader(csvfile)
 
+def read_match_data(file_path):
     match_played = {}
+    with open(file_path, newline='') as csvfile:
+        dataset = csv.DictReader(csvfile)
+        for row in dataset:
+            season = row['season']
+            team1 = row['team1']
+            team2 = row['team2']
 
-    for row in DataSet:
-        season = row['season']
-        team1 = row['team1']
-        team2 = row['team2']
+            if season not in match_played:
+                match_played[season] = {}
 
-        if season not in match_played:
-            match_played[season] = {}
-        
-        if team1 not in match_played[season]:
-            match_played[season][team1] = 1
-        else:
-            match_played[season][team1] += 1
+            if team1 not in match_played[season]:
+                match_played[season][team1] = 1
+            else:
+                match_played[season][team1] += 1
 
-        if team2 not in match_played[season]:
-            match_played[season][team2] = 1
-        else:
-            match_played[season][team2] += 1
+            if team2 not in match_played[season]:
+                match_played[season][team2] = 1
+            else:
+                match_played[season][team2] += 1
 
+    return match_played
 
-years = list(match_played.keys())
-teams = list(match_played[years[0]].keys())
+def plot_matches_played_by_team(match_data):
+    years = list(match_data.keys())
+    teams = list(match_data[years[0]].keys())
 
-# Create a bar chart
-fig, ax = plt.subplots(figsize=(12, 6))
-bottom = [0] * len(years)
+    fig, ax = plt.subplots(figsize=(12, 6))
+    bottom = [0] * len(years)
 
-for team in teams:
-    # Get the number of matches played by the team for each year
-    team_values = [match_played[year].get(team, 0) for year in years]
-    ax.bar(years, team_values, label=team, bottom=bottom)
-    bottom = [b + v for b, v in zip(bottom, team_values)]
+    for team in teams:
+        team_values = [match_data[year].get(team, 0) for year in years]
+        ax.bar(years, team_values, label=team, bottom=bottom)
+        bottom = [b + v for b, v in zip(bottom, team_values)]
 
-# Customize the plot
-ax.set(xlabel='Year', ylabel='Number of Matches', title='IPL Matches Played by Team (2008-2017)')
-plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.tight_layout()
+    ax.set(xlabel='Year', ylabel='Number of Matches', title='IPL Matches Played by Team (2008-2017)')
+    plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+    plt.tight_layout()
+    plt.show()
 
-# Display the plot
-plt.show()
+def main():
+    file_path = 'IPL/matches.csv'
+    match_data = read_match_data(file_path)
+    plot_matches_played_by_team(match_data)
 
+if __name__ == '__main__':
+    main()
