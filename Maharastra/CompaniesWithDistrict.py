a3ebf189c5797e952cb3bc4f303175fda59a943a
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-21 17:30:25 +0530

    compny standards

--- Maharastra/CompaniesWithDistrict.py
+++ Maharastra/CompaniesWithDistrict.py
@@ -2,55 +2,52 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+def load_company_data(file_path):
+    company_data = {}
+    with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
+        data_sheet = csv.DictReader(csvfile)
+        for row in data_sheet:
+            pin = row['Registered_Office_Address'][-6:]
+            date = row['DATE_OF_REGISTRATION']
+            try:
+                date_obj = datetime.strptime(date, "%d-%m-%y")
+                year = date_obj.year
+                if year == 2015 and pin not in company_data and pin != '000000':
+                    company_data[pin] = 1
+                elif year == 2015:
+                    company_data[pin] += 1
+            except ValueError:
+                pass
+    return company_data
 
-with open('Maharastra /Maharashtra.csv', newline='',encoding='iso-8859-1') as csvfile:
-    DataSheet = csv.DictReader(csvfile)
-    
-    Company_Dist = {}
-    for row in DataSheet:
-        pin = row['Registered_Office_Address'][-6:]
-        date = row['DATE_OF_REGISTRATION']
-        try:
-            date_obj = datetime.strptime(date, "%d-%m-%y")
-            year = date_obj.year
-            if year == 2015 and pin not in Company_Dist and pin != '000000':
-                
-                    Company_Dist[pin] = 1
-            elif year == 2015:
+def load_pincode_data(file_path, company_data):
+    comp_add = {}
+    with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
+        pin_code_data = csv.DictReader(csvfile)
+        for row in pin_code_data:
+            pin = row['Pin Code']
+            dist = row['District']
+            if pin in company_data:
+                count = company_data[pin]
+                if dist in comp_add:
+                    comp_add[dist] += count
+                else:
+                    comp_add[dist] = count
+    return comp_add
 
-                    Company_Dist[pin] += 1
-                    
-        except ValueError:
-            pass
+def plot_company_counts(comp_add):
+    x = list(comp_add.keys())
+    y = list(comp_add.values())
 
+    plt.bar(x, y)
+    plt.xlabel('Districts')
+    plt.ylabel('Company Counts')
+    plt.title('Company Counts by District')
+    plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
+    plt.tight_layout()  # Adjust the layout for better presentation
+    plt.show()
 
-
-with open('Maharastra /pincode.csv', newline='',encoding='iso-8859-1') as csvfile:
-    PIN_CODE = csv.DictReader(csvfile)
-    Comp_add = {}
-    for row in PIN_CODE:
-        pin = row['Pin Code']
-        dist = row['District']
-        
-        if pin in Company_Dist:
-            cnt = Company_Dist[pin]
-            # Accumulate the counts for the same pin code
-            if dist in Comp_add:
-                Comp_add[dist] += cnt
-            else:
-                Comp_add[dist] = cnt
-
-
-
-x = list(Comp_add.keys())
-y = list(Comp_add.values())
-
-plt.bar(x, y)
-plt.xlabel('Districts')
-plt.ylabel('Company Counts')
-plt.title('Company Counts by District')
-plt.xticks(rotation=90)  # Rotate x-axis labels for better visibility
-plt.tight_layout()  # Adjust the layout for better presentation
-
-plt.show()
-
+if __name__ == '__main__':
+    company_data = load_company_data('Maharastra /Maharashtra.csv')
+    comp_add = load_pincode_data('Maharastra /pincode.csv', company_data)
+    plot_company_counts(comp_add)
