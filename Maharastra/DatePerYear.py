be0ab6675c886e3d01c32673bb05035a0530f1c6
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-21 17:41:35 +0530

    new updates

--- Maharastra/DatePerYear.py
+++ Maharastra/DatePerYear.py
@@ -1,37 +1,36 @@
 import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 file_path = 'Maharastra /Maharashtra.csv'
+
+
+# Use a defaultdict for storing registration counts per year
+Company = defaultdict(int)
 
 with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
     Dataset = csv.DictReader(csvfile)
 
-    Company = {}
-
     for row in Dataset:
-        
         date = row['DATE_OF_REGISTRATION']
 
         try:
             date_obj = datetime.strptime(date, "%d-%m-%y")
             year = date_obj.year
-            if year >2025:
-                year = year - 100
+            if year >= 2025:  # Subtract 100 for years greater than or equal to 2025
+                year -= 100
         except ValueError:
             pass
-        
-        if year not in Company: 
-            Company[year] = 1
-        else:
-            Company[year] += 1
-            
-            
-           
 
+        Company[year] += 1
 
+# Sort the dictionary by year
 sorted_dict = dict(sorted(Company.items()))
 
-plt.bar(sorted_dict.keys(),sorted_dict.values())
+# Plotting the data
+plt.bar(sorted_dict.keys(), sorted_dict.values())
+plt.xlabel('Year')
+plt.ylabel('Number of Registrations')
+plt.title('Company Registrations in Maharashtra by Year')
 plt.show()
-
