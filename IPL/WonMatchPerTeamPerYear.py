a517dde7795656ba79275a28eecf4d98dac7bde5
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-21 17:26:44 +0530

    updated

--- IPL/WonMatchPerTeamPerYear.py
+++ IPL/WonMatchPerTeamPerYear.py
@@ -1,62 +1,45 @@
-import csv 
+import csv
 import matplotlib.pyplot as plt
-with open('IPL/matches.csv') as csvfile: 
-    DataSet = csv.DictReader(csvfile)
-    
-    Winners = {}
-    
-    for row in DataSet:
-        season = row['season']
-        winner = row['winner']
 
-        if season not in Winners:
-            Winners[season] = {}
+def load_data(filename):
+    data = {}
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            season = row['season']
+            winner = row['winner']
+            if season not in data:
+                data[season] = {}
+            if winner not in data[season]:
+                data[season][winner] = 1
+            else:
+                data[season][winner] += 1
+    return data
 
-        if winner not in Winners[season]:
-            Winners[season][winner] = 1
-        else:
-            Winners[season][winner] += 1
+def prepare_data(data):
+    sorted_data = dict(sorted(data.items(), key=lambda x: x[0]))
+    teams = list(set(winner for season_data in sorted_data.values() for winner in season_data.keys()))
+    team_wins = {team: [year_data.get(team, 0) for year_data in sorted_data.values()] for team in teams}
+    return teams, team_wins
 
-            
-    sorted_matches_played = dict(sorted(Winners.items(),key=lambda x : x[0]))
-    
-    list_teams = []
-    for season_data in sorted_matches_played.values():
-        for team in season_data.keys():
-            if team not in list_teams:
-                list_teams.append(team)
-                
-    team_wins = {team: [year_data.get(team, 0) for year_data in sorted_matches_played.values()] for team in list_teams}
-    values = list(team_wins.values())
-    positions = [season for season in range(2008,2018)]
+def plot_bar_chart(teams, team_wins):
+    positions = [str(year) for year in range(2008, 2018)]
     color_codes = [
-        "#3366FF",  # Blue
-        "#FFFF33",  # Yellow
-        "#FF5733",  # Red 
-        "#33FF57",  # Green
-        "#FF33FF",  # Pink
-        "#33FFFF",  # Cyan
-        "#FF9933",  # Orange
-        "#9933FF",  # Purple
-        "#33FF99",  # Teal
-        "#FF33CC",  # Magenta
-        "#33CCFF",  # Sky Blue
-        "#FFCC33",  # Gold
-        "#33FFCC",  # Sea Green
-        "#CC33FF",  # Lavender
-        "#33CC99",  # Forest Green
+        "#3366FF", "#FFFF33", "#FF5733", "#33FF57", "#FF33FF", "#33FFFF", "#FF9933", "#9933FF",
+        "#33FF99", "#FF33CC", "#33CCFF", "#FFCC33", "#33FFCC", "#CC33FF", "#33CC99"
     ]
     plt.figure(figsize=(10, 5))
-    bottom = [0]*len(positions)
-    for i in range(len(list_teams)):
-        plt.bar(positions,values[i],color = color_codes[i],label=list_teams[i],bottom=bottom)
-        bottom = [bottom[j] + values[i][j] for j in range(len(positions))]
-    
-    
-    plt.legend()
+    bottom = [0] * len(positions)
+    for i, team in enumerate(teams):
+        plt.bar(positions, team_wins[team], color=color_codes[i], label=team, bottom=bottom)
+        bottom = [bottom[j] + team_wins[team][j] for j in range(len(positions))]
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.xlabel('Season')
+    plt.ylabel('Wins')
+    plt.title('IPL Team Wins Over Seasons')
     plt.show()
-    
 
-
-                
-                
+if __name__ == '__main__':
+    data = load_data('IPL/matches.csv')
+    teams, team_wins = prepare_data(data)
+    plot_bar_chart(teams, team_wins)
