b6a558239c7bb03f67a1264e8b70000777b1b709
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-21 17:42:01 +0530

    latest file

--- Maharastra/HistogramofCapital.py
+++ Maharastra/HistogramofCapital.py
@@ -1,31 +1,40 @@
 import csv
 import matplotlib.pyplot as plt
 
-file_path = 'Maharastra /Maharashtra.csv'
+def load_data(file_path):
+    histogram = {'<= 1L': 0, '1L to 10L': 0, '10L to 1Cr': 0, '1Cr to 10Cr': 0, '> 10Cr': 0}
 
-with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
-    Dataset = csv.DictReader(csvfile)
+    with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
+        data_reader = csv.DictReader(csvfile)
 
-    Histogram = {'<= 1L': 0, '1L to 10L': 0, '10L to 1Cr': 0, '1Cr to 10Cr': 0, '> 10Cr': 0}
+        for row in data_reader:
+            val = float(row['AUTHORIZED_CAP'])
+            if val <= 100000:
+                histogram['<= 1L'] += 1
+            elif 100000 < val <= 1000000:
+                histogram['1L to 10L'] += 1
+            elif 1000000 < val <= 10000000:
+                histogram['10L to 1Cr'] += 1
+            elif 10000000 < val <= 100000000:
+                histogram['1Cr to 10Cr'] += 1
+            else:
+                histogram['> 10Cr'] += 1
 
-    for row in Dataset:
-        val = float(row['AUTHORIZED_CAP'])
-        if val <= 100000:
-            Histogram['<= 1L'] += 1
-        elif 100000 < val <= 1000000:
-            Histogram['1L to 10L'] += 1
-        elif 1000000 < val <= 10000000:
-            Histogram['10L to 1Cr'] += 1
-        elif 10000000 < val <= 100000000:
-            Histogram['1Cr to 10Cr'] += 1
-        else:
-            Histogram['> 10Cr'] += 1
+    return histogram
 
-y_labels = list(Histogram.keys())
-data = list(Histogram.values())
+def plot_histogram(histogram_data):
+    categories = list(histogram_data.keys())
+    counts = list(histogram_data.values())
 
-bin = len(y_labels)
+    num_categories = len(categories)
+    plt.bar(categories, counts, color='skyblue', edgecolor='red')
+    plt.xlabel('Authorized Capital Ranges')
+    plt.ylabel('Company Counts')
+    plt.title('Company Counts by Authorized Capital Range')
+    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better visibility
+    plt.show()
 
-plt.hist(y_labels,bins=bin,weights=data,edgecolor= 'red',color='skyblue')
-plt.show()
-
+if __name__ == '__main__':
+    file_path = 'Maharastra /Maharashtra.csv'
+    histogram_data = load_data(file_path)
+    plot_histogram(histogram_data)
