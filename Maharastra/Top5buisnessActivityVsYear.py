5615a5d60d9491424b3d0f45a8cbf14a582c5b13
ANKIT SHARMA <100486645+ANKIT-777@users.noreply.github.com>
2023-10-21 17:43:55 +0530

    latest updates

--- Maharastra/Top5buisnessActivityVsYear.py
+++ Maharastra/Top5buisnessActivityVsYear.py
@@ -1,77 +1,86 @@
 import csv
 import matplotlib.pyplot as plt
 
-with open('Maharastra /Maharashtra.csv', newline='', encoding='iso-8859-1') as csvfile:
-    Dataset = csv.DictReader(csvfile)
+def extract_data(file_path):
+    # Extract data from the CSV file
     Activities = {}
-    for row in Dataset:
-        date = row['DATE_OF_REGISTRATION'][-2:]
-        activity = row['PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN']
+    with open(file_path, newline='', encoding='iso-8859-1') as csvfile:
+        Dataset = csv.DictReader(csvfile)
+        for row in Dataset:
+            date = row['DATE_OF_REGISTRATION'][-2:]
+            activity = row['PRINCIPAL_BUSINESS_ACTIVITY_AS_PER_CIN']
 
-        # Check if date is 'NA' and skip this row
-        if date == 'NA':
-            continue
+            if date == 'NA':
+                continue
 
-        if int(date) > 21:
-            newdate = '19' + date
-        else:
-            newdate = '20' + date
-
-        if 2000 <= int(newdate) <= 2010:
-            if newdate not in Activities:
-                Activities[newdate] = {}
-
-            if activity not in Activities[newdate]:
-                Activities[newdate][activity] = 1
+            if int(date) > 21:
+                newdate = '19' + date
             else:
-                Activities[newdate][activity] += 1
+                newdate = '20' + date
 
-sorted_data = {date: dict(sorted(data.items(), key=lambda item: item[1], reverse=True)) for date, data in Activities.items()}
+            if 2000 <= int(newdate) <= 2010:
+                if newdate not in Activities:
+                    Activities[newdate] = {}
 
-years = list(sorted_data.keys())
-activity_year = list(sorted_data.values())
-top_activity_year = {}
+                if activity not in Activities[newdate]:
+                    Activities[newdate][activity] = 1
+                else:
+                    Activities[newdate][activity] += 1
 
-for year_index, year_activities in enumerate(activity_year):
-    top_activities = list(year_activities)[:5]
-    if years[year_index] not in top_activity_year:
-        top_activity_year[years[year_index]] = top_activities
+    return Activities
 
-# Prepare data for the grouped bar chart
-all_activity_names = set()
-for year in top_activity_year:
-    for activity in top_activity_year[year]:
-        all_activity_names.add(activity)
+def transform_data(Activities):
+    # Transform data by selecting the top activities for each year
+    sorted_data = {date: dict(sorted(data.items(), key=lambda item: item[1], reverse=True)) for date, data in Activities.items()}
 
-all_activity_names = sorted(all_activity_names)
+    top_activity_year = {}
+    for year, year_activities in sorted_data.items():
+        top_activities = list(year_activities)[:5]
+        top_activity_year[year] = top_activities
 
-bar_data = {}
-for activity in all_activity_names:
-    bar_data[activity] = []
+    return top_activity_year
+
+def prepare_data_for_plot(top_activity_year):
+    # Prepare data for the grouped bar chart
+    all_activity_names = set()
     for year in top_activity_year:
-        if activity in top_activity_year[year]:
-            bar_data[activity].append(activity_year[years.index(year)][activity])
-        else:
-            bar_data[activity].append(0)
+        for activity in top_activity_year[year]:
+            all_activity_names.add(activity)
 
-# Set the bar positions
-x = list(range(len(top_activity_year)))
+    all_activity_names = sorted(all_activity_names)
 
-# Create a bar for each activity
-width = 0.10
-fig, ax = plt.subplots()
+    bar_data = {}
+    for activity in all_activity_names:
+        bar_data[activity] = []
+        for year in top_activity_year:
+            if activity in top_activity_year[year]:
+                bar_data[activity].append(Activities[year][activity])
+            else:
+                bar_data[activity].append(0)
 
-for i, activity in enumerate(bar_data):
-    ax.bar([pos + width * i for pos in x], bar_data[activity], width=width, label=activity)
+    return bar_data, top_activity_year
 
-# Set the labels and title of the chart
-ax.set_xticks([pos + width * 2 for pos in x])
-ax.set_xticklabels(top_activity_year.keys())
-ax.set_ylabel("Number of Companies")
-ax.set_title("Top 5 Activities in Maharashtra by Number of Companies (2000-2010)")
+def plot_data(bar_data, top_activity_year):
+    # Plot the data
+    x = list(range(len(top_activity_year)))
+    width = 0.10
+    fig, ax = plt.subplots()
 
-# Add a legend
-ax.legend(loc='upper right', bbox_to_anchor=(1, 1))
+    for i, activity in enumerate(bar_data):
+        ax.bar([pos + width * i for pos in x], bar_data[activity], width=width, label=activity)
 
-# Show the chart
-plt.show()
+    ax.set_xticks([pos + width * 2 for pos in x])
+    ax.set_xticklabels(top_activity_year.keys())
+    ax.set_ylabel("Number of Companies")
+    ax.set_title("Top 5 Activities in Maharashtra by Number of Companies (2000-2010)")
+    ax.legend(loc='upper right', bbox_to_anchor=(1, 1))
+    plt.show()
+
+if __name__ == '__main__':
+    file_path = 'Maharastra /Maharashtra.csv'  # Adjust the file path
+
+    # ETL Process
+    Activities = extract_data(file_path)
+    top_activity_year = transform_data(Activities)
+    bar_data, top_activity_year = prepare_data_for_plot(top_activity_year)
+    plot_data(bar_data, top_activity_year)
